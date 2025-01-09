2715b205ad4581c6c174d6086b632afa8cafbb10
Miske9 <124712586+Miske9@users.noreply.github.com>
2025-01-09 21:43:30 +0100

    go jezik

--- igra.go
+++ igra.go
@@ -6,17 +6,22 @@ import (
 	"os"
 	"os/exec"
 	"time"
+
+	"github.com/eiannone/keyboard"
 )
 
+// Konstantne vrijednosti za veličinu ploče
 const (
 	WIDTH  = 20
 	HEIGHT = 10
 )
 
+// Struktura koja predstavlja točku (x, y) na ploči
 type Point struct {
 	x, y int
 }
 
+// clearScreen briše ekran
 func clearScreen() {
 	cmd := exec.Command("clear")
 	if os.Getenv("OS") == "Windows_NT" {
@@ -26,39 +31,39 @@ func clearScreen() {
 	cmd.Run()
 }
 
+// createBoard kreira dvodimenzionalnu ploču s okvirima (#)
 func createBoard() [][]rune {
-	board := make([][]rune, HEIGHT)
+	board := make([][]rune, HEIGHT+2)
 	for i := range board {
-		board[i] = make([]rune, WIDTH)
+		board[i] = make([]rune, WIDTH+2)
 		for j := range board[i] {
-			board[i][j] = ' '
+			if i == 0 || i == HEIGHT+1 || j == 0 || j == WIDTH+1 {
+				board[i][j] = '#' // Zidovi
+			} else {
+				board[i][j] = ' ' // Prazan prostor
+			}
 		}
 	}
 	return board
 }
 
+// printBoard iscrtava ploču i rezultat na konzoli
 func printBoard(board [][]rune, score int) {
 	clearScreen()
-	fmt.Println("+" + string(make([]rune, WIDTH, WIDTH+2)))
 	for _, row := range board {
-		fmt.Print("|")
 		for _, cell := range row {
-			if cell == 0 {
-				fmt.Print(" ")
-			} else {
-				fmt.Printf("%c", cell)
-			}
+			fmt.Printf("%c", cell)
 		}
-		fmt.Println("|")
+		fmt.Println()
 	}
-	fmt.Println("+" + string(make([]rune, WIDTH, WIDTH+2)))
-	fmt.Printf("Score: %d\n", score)
+	fmt.Printf("Rezultat: %d\n", score)
 }
 
+// placeFood postavlja hranu na slučajnu poziciju na ploči koja nije zauzeta zmijom
 func placeFood(board [][]rune, snake []Point) Point {
 	for {
-		x := rand.Intn(WIDTH)
-		y := rand.Intn(HEIGHT)
+		x := rand.Intn(WIDTH) + 1
+		y := rand.Intn(HEIGHT) + 1
 		isOnSnake := false
 		for _, p := range snake {
 			if p.x == x && p.y == y {
@@ -67,105 +72,130 @@ func placeFood(board [][]rune, snake []Point) Point {
 			}
 		}
 		if !isOnSnake {
-			board[y][x] = '*'
+			board[y][x] = '*' // Hrana
 			return Point{x, y}
 		}
 	}
 }
 
-func moveSnake(snake []Point, direction string) []Point {
+// moveSnake pomiče zmiju na novu poziciju prema trenutnom smjeru
+func moveSnake(snake []Point, xSpeed, ySpeed int) []Point {
 	head := snake[0]
-	var newHead Point
-	switch direction {
-	case "UP":
-		newHead = Point{head.x, head.y - 1}
-	case "DOWN":
-		newHead = Point{head.x, head.y + 1}
-	case "LEFT":
-		newHead = Point{head.x - 1, head.y}
-	case "RIGHT":
-		newHead = Point{head.x + 1, head.y}
-	}
-	newSnake := append([]Point{newHead}, snake...)
-	return newSnake[:len(snake)]
+	newHead := Point{head.x + xSpeed, head.y + ySpeed}
+	return append([]Point{newHead}, snake[:len(snake)-1]...)
 }
 
-func checkCollision(snake []Point) bool {
+// checkCollision provjerava sudare zmije sa zidovima ili sobom
+func checkCollision(snake []Point, board [][]rune) bool {
 	head := snake[0]
-	if head.x < 0 || head.x >= WIDTH || head.y < 0 || head.y >= HEIGHT {
+	if board[head.y][head.x] == '#' { // Sudar sa zidom
 		return true
 	}
 	for _, p := range snake[1:] {
-		if head == p {
+		if head == p { // Sudar sa vlastitim tijelom
 			return true
 		}
 	}
 	return false
 }
 
-func gameLoop() {
+// gameLoop upravlja logikom jedne igre
+func gameLoop() bool {
+	// Inicijalizacija zmije, brzine i ploče
 	snake := []Point{{WIDTH / 2, HEIGHT / 2}}
-	direction := "RIGHT"
+	xSpeed, ySpeed := 1, 0 // Početni smjer: desno
 	board := createBoard()
 	food := placeFood(board, snake)
 	score := 0
 
-	// Goroutine za kontrolu unosa korisnika
-	inputChan := make(chan string)
+	// Otvaranje praćenja tipkovnice
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	defer keyboard.Close()
+
+	// Kreiranje kanala za unos
+	inputChan := make(chan rune)
 	go func() {
-		var key string
 		for {
-			fmt.Scanln(&key)
-			inputChan <- key
+			if char, _, err := keyboard.GetKey(); err == nil {
+				inputChan <- char
+			}
 		}
 	}()
 
 	for {
-		// Resetiraj ploču i postavi zmiju i hranu
+		// Pomiče zmiju u trenutnom smjeru
+		snake = moveSnake(snake, xSpeed, ySpeed)
+
+		// Provjerava sudar
+		if checkCollision(snake, board) {
+			printBoard(board, score)
+			fmt.Println("Game Over!")
+			fmt.Println("Double click 'r' for restart or 'q' for exit.")
+			for {
+				select {
+				case char := <-inputChan:
+					if char == 'r' {
+						return true // Ponovno pokretanje
+					} else if char == 'q' {
+						return false // Izlaz
+					}
+				}
+			}
+		}
+
+		// Provjerava je li zmija pojela hranu
+		if snake[0].x == food.x && snake[0].y == food.y {
+			snake = append(snake, snake[len(snake)-1]) // Dodaje dio zmije
+			food = placeFood(board, snake)             // Nova hrana
+			score++
+		}
+
+		// Resetira ploču i dodaje zmiju i hranu
 		board = createBoard()
 		for _, p := range snake {
 			board[p.y][p.x] = 'O'
 		}
 		board[food.y][food.x] = '*'
 
-		// Prikaži ploču
+		// Prikazuje ploču
 		printBoard(board, score)
 
-		// Provjera unosa korisnika
+		// Pauza kako bi igra imala stabilnu brzinu
+		time.Sleep(200 * time.Millisecond)
+
+		// Obrada unosa za smjer kretanja
 		select {
-		case key := <-inputChan:
-			if key == "w" && direction != "DOWN" {
-				direction = "UP"
-			} else if key == "s" && direction != "UP" {
-				direction = "DOWN"
-			} else if key == "a" && direction != "RIGHT" {
-				direction = "LEFT"
-			} else if key == "d" && direction != "LEFT" {
-				direction = "RIGHT"
+		case char := <-inputChan:
+			switch char {
+			case 'w':
+				if ySpeed == 0 {
+					xSpeed, ySpeed = 0, -1
+				}
+			case 's':
+				if ySpeed == 0 {
+					xSpeed, ySpeed = 0, 1
+				}
+			case 'a':
+				if xSpeed == 0 {
+					xSpeed, ySpeed = -1, 0
+				}
+			case 'd':
+				if xSpeed == 0 {
+					xSpeed, ySpeed = 1, 0
+				}
 			}
 		default:
-			// Nastavi bez unosa
+			// Nastavlja kretanje bez promjene
 		}
-
-		// Pomakni zmiju
-		snake = moveSnake(snake, direction)
-		if checkCollision(snake) {
-			fmt.Println("Game Over! Your score:", score)
-			break
-		}
-
-		// Provjeri je li zmija pojela hranu
-		if snake[0].x == food.x && snake[0].y == food.y {
-			snake = append(snake, Point{}) // Dodaj segment
-			food = placeFood(board, snake) // Postavi novu hranu
-			score++
-		}
-
-		time.Sleep(200 * time.Millisecond)
 	}
 }
 
+// main upravlja početkom i ponovnim pokretanjem igre
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	gameLoop()
+	rand.Seed(time.Now().UnixNano()) // Postavlja seed za slučajne brojeve
+	for gameLoop() {
+		// Ponovno pokreće igru ako gameLoop vrati true
+	}
 }
